e3260b55c61d7baffcda3682b2c9610dbbec82e1
MatthewBert <47572136+MatthewBert@users.noreply.github.com>
2024-05-31 11:27:46 -0400

    books now display on the webpage

--- script.js
+++ script.js
@@ -1,14 +1,43 @@
-function Book(title,author,pages,read){
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
+const myLibrary = [];
 
-    this.info = function(){
-        let info = this.title + " by " + this.author + ", " + this.pages + " pages, " + this.read;
-        return info;
-    }
+function Book(title, author, pages, isRead) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.isRead = isRead;
 }
 
-const theHobbit = new Book("The Hobbit", "J.R.R Tolkien", 295, "not ready yet");
-console.log(theHobbit.info());
+addBookToLibrary('Hamlet', 'William Shakespeare', 432, true);
+addBookToLibrary('The Hobbit', 'J.R.R Tolkien', 295, false);
+addBookToLibrary('50 Below Zero', 'Robert Munsch', 24, true);
+
+function addBookToLibrary(title, author, pages, isRead) {
+    const newBook = new Book(title, author, pages, isRead);
+    myLibrary.push(newBook);
+}
+
+function displayBook(){
+    const bookshelf = document.getElementById("bookshelf");
+
+    myLibrary.forEach(book => {
+        const card = document.createElement("div");
+        card.classList.add("card");
+
+        const title = document.createElement("h2");
+        title.textContent = book.title;
+
+        const author = document.createElement("p");
+        author.textContent = "Author: " + book.author;
+
+        const pages = document.createElement("p");
+        pages.textContent = "Pages: " + book.pages;
+    
+        card.appendChild(title);
+        card.appendChild(author);
+        card.appendChild(pages);
+
+        bookshelf.appendChild(card);
+    });
+}
+
+window.onload = displayBook;
