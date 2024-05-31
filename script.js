ac66a2ff1c7bc41c6ce66431a512bf3af3c14e5a
MatthewBert <47572136+MatthewBert@users.noreply.github.com>
2024-05-31 13:23:17 -0400

    NEW BOOK button added to add books to the library

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const myLibrary = [];
 
+// Constructor
 function Book(title, author, pages, isRead) {
   this.title = title;
   this.author = author;
@@ -11,11 +12,13 @@ addBookToLibrary('Hamlet', 'William Shakespeare', 432, true);
 addBookToLibrary('The Hobbit', 'J.R.R Tolkien', 295, false);
 addBookToLibrary('50 Below Zero', 'Robert Munsch', 24, true);
 
+// Adds books to myLibrary array
 function addBookToLibrary(title, author, pages, isRead) {
     const newBook = new Book(title, author, pages, isRead);
     myLibrary.push(newBook);
 }
 
+// Display books to the DOM 
 function displayBook(){
     const bookshelf = document.getElementById("bookshelf");
 
@@ -40,4 +43,45 @@ function displayBook(){
     });
 }
 
-window.onload = displayBook;
+function resetBookShelf(){
+    const bookshelf = document.getElementById("Bookshelf");
+    myLibrary.forEach(book => {
+        const card = document.querySelector(".card");
+        card.remove();
+    });
+}
+
+// Dialog related functions
+function showDialog(){
+    document.querySelector('#mainDialog').style.display = 'block';
+}
+
+function closeDialog(){
+    document.querySelector('#mainDialog').style.display = 'none';
+}
+
+document.querySelector('.addButton').onclick = showDialog;
+document.querySelector('#closeDialog').onclick = closeDialog;
+
+
+// Form handler
+document.querySelector('#bookForm').onsubmit = function(e){
+    e.preventDefault();
+
+    const title = document.querySelector('#title').value;
+    const author = document.querySelector('#author').value;
+    const pages = document.querySelector('#pages').value;
+    const isRead = document.querySelector('#isRead').checked;
+
+    if (title && author && !isNaN(pages) && pages >= 1) {
+        resetBookShelf();
+        addBookToLibrary(title, author, pages, isRead);
+        displayBook();
+        closeDialog();
+        document.querySelector('#bookForm').reset();
+      } else {
+        alert('Please fill in all fields correctly.');
+      }
+}
+
+window.onload = displayBook();
